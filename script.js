e0e6c167c03c41e3c355d5fb3511af84fa546414
Imu Sama <yowina.megafujoshi67@gmail.com>
2026-05-03 09:37:26 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// 🔍 BUSCA
+const search = document.getElementById("search");
+
+if (search) {
+  search.addEventListener("input", () => {
+    const value = search.value.toLowerCase();
+    document.querySelectorAll(".card").forEach(card => {
+      card.style.display = card.innerText.toLowerCase().includes(value)
+        ? "block"
+        : "none";
+    });
+  });
+}
