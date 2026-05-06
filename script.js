ef360d40a335a7fbbd91e7c9e275d151bc9e7502
Imu Sama <yowina.megafujoshi67@gmail.com>
2026-05-06 18:12:53 -0400

    Refactor search input event listener syntax

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// 🔍 BUSCA
 const search = document.getElementById("search");
 
-if (search) {
-  search.addEventListener("input", () => {
+if(search){
+  search.addEventListener("input",()=>{
     const value = search.value.toLowerCase();
-    document.querySelectorAll(".card").forEach(card => {
+
+    document.querySelectorAll(".card").forEach(card=>{
       card.style.display = card.innerText.toLowerCase().includes(value)
         ? "block"
         : "none";
